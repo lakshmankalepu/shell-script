692f812cc18488d43dd7d8afccf1d7975af8afd1
lakshmankalepu <kalepulakshman@outlook.com>
2024-04-18 00:39:02 +0530

    write-something

--- 02-variable.sh
+++ 02-variable.sh
@@ -1,1 +1,7 @@
-this is again a sample shell script
+#!/bin/bash
+
+person1=ramesh
+person2=suresh
+
+echo "$person1:: hi $person2, how are you?"
+echo "$person2:: hi, i am fine, how are you $person1?"
